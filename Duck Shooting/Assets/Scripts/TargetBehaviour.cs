5f4ef555cfa4b71781a4d4cb0b26aac8997a1cc0
RobertHC <roberthcarleton@gmail.com>
2017-03-30 18:46:50 -0700

    Duck shooting game

---
+++ Duck Shooting/Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour {
+
+    private bool beenHit = false;
+    private Animator animator;
+    private GameObject parent;
+
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+        animator = parent.GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Called whenever the player clicks on the object.
+    /// only works if you have a collider on the object
+    /// </summary>
+    void OnMouseDown()
+    {
+        // Is it valid to hit it
+        if (!beenHit)
+        {
+            beenHit = true;
+            animator.Play("Flip");
+        }
+    }
+}
